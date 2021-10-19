0bac66a6661613f8ebaa49232f8d04685a44aeef
Anshaal Hussain <anshaalhussain@gmail.com>
2021-10-19 19:15:04 -0400

    fixing bugs from previous merge

--- server.js
+++ server.js
@@ -39,14 +39,12 @@ app.use(
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const authRoutes = require("./routes/auth");
 const resourceRoutes = require("./routes/resources");
 const homeRoutes = require("./routes/home-page");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/", authRoutes());
+app.use("/", authRoutes);
 app.use("/home", homeRoutes(db));
 app.use("/api/users", usersRoutes(db));
 
