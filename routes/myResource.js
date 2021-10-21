a691d87d778d2daa2861f912507ec072c077b3e6
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-10-21 19:15:12 -0400

    Search the resources by title and show the saved resources by user

---
+++ routes/myResource.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const checkAuth = require("../middlewares/check-auth");
+
+module.exports = (db) => {
+
+  const searchAllResourcesByID = function(userId) {
+    let query = `SELECT resources.url, resources.title, categories.category_name FROM resources
+    JOIN categories ON categories.id = category_id
+    WHERE resources.user_id = $1`;
+
+    return db.query(query, [userId])
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log("Search query err.message:", err.message);
+      });
+  };
+
+  router.get("/", checkAuth, (req, res) => {
+    const userId = req.currentUser.id;
+    const user = req.currentUser;
+    const templateVars = {
+      name: user.name,
+      email: user.email,
+      userId: user.id,
+    };
+    searchAllResourcesByID(userId).then((rows) => {
+      templateVars["rows"] = rows;
+      console.log("templateVars", templateVars);
+      if (req.currentUser) {
+        res.render("myResource", templateVars);
+      } else {
+        res.redirect("/login");
+      }
+    });
+  });
+
+  return router;
+};
---
+++ routes/search.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const checkAuth = require("../middlewares/check-auth");
+
+module.exports = (db) => {
+
+  const searchResourceByTitle = function(userId, title) {
+    let query = `SELECT resources.url, resources.title, categories.category_name FROM resources
+    JOIN categories ON categories.id = category_id
+    WHERE resources.user_id = $1 AND resources.title = $2`;
+
+    return db.query(query, [userId, title])
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  router.get("/", checkAuth, (req, res) => {
+    const title = req.body.search;
+    //const userId = req.currentUser.id;
+    const userId = 15;
+    const user = req.currentUser;
+    const templateVars = {
+      name: user.name,
+      email: user.email,
+      userId: user.id,
+    };
+    searchResourceByTitle(userId, title).then((rows) => {
+      templateVars["rows"] = rows[0];
+      console.log("templateVars", templateVars);
+      if (req.currentUser) {
+        res.render("search", templateVars);
+      } else {
+        res.send("Sorry, resource could not be found by title");
+      }
+    });
+  });
+
+  return router;
+};
