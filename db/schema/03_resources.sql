c167c7b38178bc949e05ffe19aaf7dac932e69db
Nancy Boktor <nancyboktor1@gmail.com>
2021-10-21 17:48:08 -0400

    adding to db a new coloumn for images as avantica categeory

--- db/schema/03_resources.sql
+++ db/schema/03_resources.sql
@@ -5,6 +5,7 @@ CREATE TABLE resources (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  image VARCHAR(max);
   url VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT
