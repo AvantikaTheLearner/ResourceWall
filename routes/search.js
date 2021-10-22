a29340e31a3c8b7215ef33ca73437c4cd234279b
Anshaal Hussain <anshaalhussain@gmail.com>
2021-10-22 00:58:53 -0400

    some styling changes to my resource page, bug fix to search function

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,7 @@ const checkAuth = require("../middlewares/check-auth");
 
 module.exports = (db) => {
 
-  const searchResourceByTitle = function(userId, title) {
+  const searchResourceByTitle = function (userId, title) {
     let query = `SELECT resources.image, resources.url, resources.title, categories.category_name FROM resources
     JOIN categories ON categories.id = category_id
     WHERE resources.user_id = $1 AND resources.title = $2`;
@@ -20,23 +20,26 @@ module.exports = (db) => {
 
   router.get("/", checkAuth, (req, res) => {
     const title = req.body.search;
-    //const userId = req.currentUser.id;
-    const userId = 15;
+    const userId = req.currentUser.id;
+    // const userId = 15;
     const user = req.currentUser;
     const templateVars = {
       name: user.name,
       email: user.email,
       userId: user.id,
     };
-    searchResourceByTitle(userId, title).then((rows) => {
-      templateVars["rows"] = rows[0];
-      console.log("templateVars", templateVars);
-      if (req.currentUser) {
-        res.render("search", templateVars);
-      } else {
-        res.send("Sorry, resource could not be found by title");
-      }
-    });
+
+    searchResourceByTitle(userId, title)
+
+      .then((rows) => {
+        templateVars["rows"] = rows;
+        console.log("templateVars", templateVars);
+        if (req.currentUser.id) {
+          res.render("search", templateVars);
+        } else {
+          res.send("Sorry, resource could not be found by title");
+        }
+      });
   });
 
   return router;
