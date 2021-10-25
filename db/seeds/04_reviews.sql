f2c5809cc1a24400676d6a86199135eb9884c6a9
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-10-25 14:36:15 -0400

    Intermediate changes for implementing like functionality

--- db/seeds/04_reviews.sql
+++ db/seeds/04_reviews.sql
@@ -1,16 +1,16 @@
 -- reviews table seeds here (Example)
 
-INSERT INTO reviews (user_id, resource_id, rating, comment, hit_like)
+INSERT INTO reviews (user_id, resource_id, rating, comment)
 VALUES
-(1, 1, 4, 'comment', 12),
-(1, 2, 3, 'comment', 34),
-(1, 3, 2, 'comment', 23),
-(2, 1, 1, 'comment', 56),
-(2, 2, 3, 'comment', 67),
-(2, 3, 4, 'comment', 67),
-(3, 1, 2, 'comment', 78),
-(3, 2, 3, 'comment', 76),
-(4, 1, 2, 'comment', 34),
-(4, 2, 1, 'comment', 56);
+(15, 1, 4, 'comment'),
+(15, 2, 3, 'comment'),
+(15, 3, 2, 'comment'),
+(15, 1, 1, 'comment'),
+(15, 2, 3, 'comment'),
+(15, 3, 4, 'comment'),
+(15, 1, 2, 'comment'),
+(15, 2, 3, 'comment'),
+(15, 1, 2, 'comment'),
+(15, 2, 1, 'comment');
 
 
